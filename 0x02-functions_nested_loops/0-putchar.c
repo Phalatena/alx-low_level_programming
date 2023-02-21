5b06c9f3eddf35a39639c7ab40940dc4b221cd14
Phalatena <mphalatena@gmail.com>
2023-02-21 12:49:20 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,17 @@
  * main - program tha print putchar
  *
  * Return: 0 (success)
- */
+*/
 int main(void)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	putchar("\n")
-		return (0);
+putchar("_");
+putchar("p");
+putchar("u");
+putchar("t");
+putchar("c");
+putchar("h");
+putchar("a");
+putchar("r");
+putchar("\n");
+return (0);
 }
