7beaa72c7eed847ad511e836aa4324bdfef0ec87
Phalatena <mphalatena@gmail.com>
2023-02-21 17:38:16 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_sign - function that print the sign of a number
+ *@n: the number to be check
+ *Return: 1 for positive num, -1 for negative num or zero for anything else
+*/
+int print_sign(int n)
+{
+		if (n > 0)
+		{
+			_putchar('+');
+			return (1);
+		} else if (n == 0)
+		{
+			_putchar(48);
+			return (0);
+		} else if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+}
