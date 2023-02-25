c92e6774ad5ad9defe9f6ca7a36022f1b267185a
Phalatena <mphalatena@gmail.com>
2023-02-24 16:57:21 -0800

    The World looks like a multiplication

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_times_table - function that print n times table starting with 0.
+ *times_table - function that print n times table starting with 0.
  *@n: The number to be checked
  *Return: Always 0
 */
