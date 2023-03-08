2835382431ac574d39d171f15832e384795eab2c
Phalatena <mphalatena@gmail.com>
2023-03-08 05:40:14 -0800

    dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_sqrt_recursion - funtion that return the natural square root of a number
+ *@n: natural square root
+ *Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (n);
+}
