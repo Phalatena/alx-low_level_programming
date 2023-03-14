9aef3a8ac6f0f1f20f5d9d5b1a0c8e413c90ec6f
Phalatena <mphalatena@gmail.com>
2023-03-14 02:43:42 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *wildcmp - funtion that compares 2 string and return 1.
+ *@s1: pointer to string
+ *@s2: compare
+ *Return: 1 if identical or 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,3 +11,17 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (n, 0);
 }
+/**
+ *actual_sqrt_recursion - recurses to find the natural
+ *@n: number to calculate the sqaure root
+ *@i: iterator
+ *Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
