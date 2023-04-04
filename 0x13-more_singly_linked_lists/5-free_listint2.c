b98b7843dd94a6e45963f793c952e6a4569afdff
Phalatena <mphalatena@gmail.com>
2023-04-04 04:13:12 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,20 +6,19 @@
 /**
  * free_listint2 - function that frees a listint_t list.
  * @head:pointer to a pointer to the first node of a linked list
- * Return: 0
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *new_node;
+	listint_t *new;
 
 	if (head == NULL)
 		return;
 
 	while (*head != NULL)
 	{
-		new_node == *head;
+		new == *head;
 		*head = (*head)->next;
-		free(new_node);
+		free(new);
 	}
 	*head = NULL;
 }
