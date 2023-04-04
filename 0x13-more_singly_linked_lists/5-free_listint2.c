5c9da658f04de8877ffb1f7781c6b3bf7f31716a
Phalatena <mphalatena@gmail.com>
2023-04-04 04:18:31 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 #include <stddef.h>
 /**
  * free_listint2 - function that frees a listint_t list.
- * @head:pointer to a pointer to the first node of a linked list
+ * @head:pointer to a pointer to the
+ * first node of a linked list.
  */
 void free_listint2(listint_t **head)
 {
